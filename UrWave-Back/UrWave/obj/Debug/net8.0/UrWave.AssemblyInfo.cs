fffa74e87df0124a96d53567d9ecfa86a20c66f4
Ahmedhasseb <ahmedhasseb310@gmail.com>
2025-01-02 02:43:25 +0200

    updateing

--- UrWave-Back/UrWave/obj/Debug/net8.0/UrWave.AssemblyInfo.cs
+++ UrWave-Back/UrWave/obj/Debug/net8.0/UrWave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrWave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d6dc25b4adb1fd7162985c9e82e7b8acbcbee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrWave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrWave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- urWave-Front/src/app/app.routes.ts
+++ urWave-Front/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:ProductComponent},
+    {path: 'Update/:id', component:UpdateProductComponent},
+    {path: 'AddProduct', component:AddProductComponent}
+
+];
